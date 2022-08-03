9200f163edc0ad6a01d85145301293a4b9dd6abf
AkaAugustoNeves <augusto.nevesti@gmail.com>
2022-08-03 17:16:35 -0300

    primeiros cursos

---
+++ 02 - Avançando com PHP - Arrays, Strings, Função e web/listas.php
@@ -0,0 +1,8 @@
+<?php
+
+$idades = [10, 11, 12, 13, 14];
+
+echo $idades[0];
+// for ($i=0; $i < $idades.sizeof() ; $i++) { 
+//     $idades[i];
+// }
---
+++ 02 - Avançando com PHP - Arrays, Strings, Função e web/loop-lista.php
@@ -0,0 +1,8 @@
+<?php
+
+$idades = [10, 11, 12, 13, 14];
+
+
+for ($i=0; $i < count($idades) ; $i++) { 
+    echo $idades[$i].PHP_EOL;
+}
